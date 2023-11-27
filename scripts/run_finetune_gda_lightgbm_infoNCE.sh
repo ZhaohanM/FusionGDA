67c35121600c956cc3978a1f0e314f20950b1365
Zhaohan Meng <111442062+ZhaohanM@users.noreply.github.com>
2023-11-27 22:58:04 +0000

    Update run_finetune_gda_lightgbm_infoNCE.sh

--- scripts/run_finetune_gda_lightgbm_infoNCE.sh
+++ scripts/run_finetune_gda_lightgbm_infoNCE.sh
@@ -13,7 +13,6 @@ for save_model_path in ${model_path_list[@]};
         for step in {1..27};
             do
             num_leaves=$((2**($max_depth-1)))
-            # cd ../src/finetune/ finetune_gda_lightgbm_infonce_cv.py
             python finetune_gda_with_adapter_lightgbm_infonce.py \
                     --save_model_path $save_model_path \
                     --device $device \
